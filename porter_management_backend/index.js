e9fbf881cee3603b01779af5ced325564452f405
Rahul Adhikari <rahul.adhikari@amniltech.com>
2025-12-07 17:54:59 +0545

    backend server setup

---
+++ porter_management_backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express(); // creation of the server
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectDB();
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Porter_management_server is running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`porter_management_server is running at ${PORT}`);
+});
